07f376029e718a3e8ef1e9a7f1b0da94cca135a7
Jorge Cimentada <cimentadaj@gmail.com>
2016-10-07 12:26:19 +0200

    Update Experimental TERCE.r

--- Experimental TERCE.r
+++ Experimental TERCE.r
@@ -10,6 +10,7 @@ unzip(paste0(direc1, "Resultados texto.zip"), exdir = paste0(direc1))
 direc2 <- paste0(direc1, "Bases de datos texto/")
 
 for (i in list.files(direc, pattern = "*.csv")) {
+  print(i)
   write_csv(empty <- read_delim(paste0(direc, i), delim = ";", col_names = T), path = paste0(direc, i))
 }
 rm(empty)
@@ -33,10 +34,27 @@ all_dat[[2]] <- Map(function(x, y) assign(x, read_csv(y, col_names = T)), vecnam
 all_dat <- lapply(all_dat, function(x) lapply(x, function(p) {names(p) <- tolower(names(p)); p}))
 all_dat <- lapply(all_dat, function(x) lapply(x, as.data.frame))
 
+finder <- c("32" = "ARG",
+            "76" = "BRA",
+            "152" = "CHL",
+            "170" = "COL",
+            "188" = "CRI",
+            "214" = "REP",
+            "218" = "ECU",
+            "320" = "GTM",
+            "340" = "HON",
+            "484" = "MEX",
+            "558" = "NIC",
+            "591" = "PAN",
+            "600" = "PAR",
+            "604" = "PER",
+            "858" = "URU",
+            "4841" = "NLE")
 
 all_dat2 <- lapply(all_dat, function(x) lapply(x, function(p) {
-  p$oID <- paste0(p$idcntry, p$idschool)
-  p$sID <- paste0(p$idcntry, p$idstud)
+  p$country <- finder[as.character(p$idcntry)]
+  p$sID <- paste0(p$country, p$idstud)
+  p$oID <- paste0(p$country, p$idschool)
   p
 }))
 
