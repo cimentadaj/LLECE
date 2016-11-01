c8ac186b9583d6e5a70bf75b9741bf0c470c1dbc
Jorge Cimentada <cimentadaj@gmail.com>
2016-10-31 20:31:23 -0400

    Update unrar.R

--- Functions/unrar.R
+++ Functions/unrar.R
@@ -1,12 +1,13 @@
 # This function uses unRAR tools for Mac OSX to
 # unrar files directly from command line
+
 # The user must have it installed by following
 # these steps: http://best-mac-tips.com/2013/02/01/install-free-command-line-unrar-mac/
-# zipfile = exact path to the zip file
+
+# rarfile = exact path to the rar file
 # exdir = directory to be extracted
 
-unrar <- function(zipfile, exdir) {
-  call <- paste("cd", exdir,"\n", "unrar x", zipfile, sep = " ")
-  print(call)
+unrar <- function(rarfile, exdir) {
+  call <- paste("cd", exdir,"\n", "unrar x", rarfile, sep = " ")
   system(call)
 }
