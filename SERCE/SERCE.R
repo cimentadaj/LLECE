4cf736ba6972e530de89d11229f1ee70ad7ad8bb
cimentadaj <cimentadaj@gmail.com>
2016-11-03 00:55:49 -0400

    Finished the section of unraring all files. The new addition checks if the first rar file containing all data sets has been unrared. If not, then it unrars that file and all the other .rar files within separate folders. The end result is that all .rar files within the first directory have been unrared in their home path.

--- SERCE/SERCE.R
+++ SERCE/SERCE.R
@@ -1,13 +1,16 @@
 library(tidyverse)
+library(downloader)
 
 # SET WORKING DIRECTORY HERE WHERE FILES WILL BE (IN CASE THEY MUST BE DOWNLOADED)
 # AND WHERE FILES ARE (IN CASE NO DOWNLOAD IS NECESSARY)
-
 setwd("/Users/cimentadaj/Downloads/serce")
 
-github_fun <- "https://raw.githubusercontent.com/cimentadaj/LLECE/master/Functions/downloader_file.R"
-source_url(github_fun,
-           sha = sha_url(github_fun))
+funs <- c("https://raw.githubusercontent.com/cimentadaj/LLECE/master/Functions/downloader_file.R",
+          "https://raw.githubusercontent.com/cimentadaj/LLECE/master/Functions/unrar.R")
+# The unrar functions relies heavily on have UnRAR installed in your OS.
+# So have a look at the link above. This script has been only tested in mac OSx.
+
+for(i in funs) source_url(i, sha = sha_url(i))
 
 downloader_file("bcf362e6.zip",
                 "http://www.unesco.org/new/fileadmin/MULTIMEDIA/FIELD/Santiago/zip/",
@@ -19,8 +22,37 @@ data_dir <- paste0(getwd(), "/SERCE/")
 # Is the unrared file nam in the folder?
 unrar_name <- grepl("Bases pa", list.files(data_dir))
 
-# If the files is not there, then unrar it
+# If the files is not there, then unrar the file.
+# After that, for all folders inside the new path
+# unrar everything inside those folders.
 if (!(any(unrar_name))) {
-  # Unrar it
-}
+  # Exact path to the rar files
+  rar_path <- paste0(data_dir, grep("SERCE.rar", list.files(data_dir), value = T))
   
+  # Unrar it and extract it in the same folder
+  unrar(rar_path, data_dir)
+
+  # New path for the unrared folder  
+  new_path <- paste0(data_dir, grep("Bases pa", list.files(data_dir), value = T), "/")
+
+  # All files within that folder
+  fls_path <- list.files(new_path)
+  # Obtain only the file names without a '.' in their names, so only the folders
+  folder_names <- fls_path[!grepl("\\.", fls_path)]
+  
+  # Create a path for each of these new folders
+  folder_path <- paste0(new_path, folder_names, "/")
+  
+  # This function lists all the .rar files within the argument 
+  # 'folder' and unrars them in the 'exdir' argument. The password
+  # argument should be used when the rar files have the same password
+  extracter <- function(folder, exdir, password = NULL) {
+    
+    # Remember to end both paths with /
+    rar_files <- list.files(folder)[grepl("\\.rar", list.files(folder))]
+    for (i in rar_files) unrar(paste0(folder, i), exdir, password)
+  }
+  
+  # Loop through each folder path and extract all rar files within each path
+  for (i in folder_path) extracter(i, i, "serce2007")
+}
