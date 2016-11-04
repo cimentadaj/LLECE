d13048903c23e0c39a7315095e5dc06e84e297a9
cimentadaj <cimentadaj@gmail.com>
2016-11-04 15:44:04 -0400

    Attempted to finally merge all datasets but I haven't been able until now. I provide a description of the problem in the script. In a nutshell, I'm missing a unique ID for the teachers with which I could match students to teachers instead of students TO CLASSROOMS(which might have more than 1 teacher).

--- SERCE/SERCE.R
+++ SERCE/SERCE.R
@@ -89,18 +89,86 @@ all_paths[[3]] <- setdiff(all_paths_vec, c(all_paths[[1]], all_paths[[2]]))
 
 all_data <- lapply(all_paths, function(x) lapply(x, read_spss))
 
+joiner <- function(x, y, key_vars, ...) {
+  
+  # Loop through all key variables and whenever
+  # a key variable is present in both data frames,
+  # stop the loop and assign the key variable to vector 'key'.
+  # If a key variable is in neither
+  # data frames, continue to the next key variable
+  for (i in key_vars) {
+    if (i %in% names(x) && i %in% names(y)) { 
+      key <- i; break
+    } else {
+      next
+    }
+  }
+  
+  # Merge data frame with the common key
+  df <- full_join(x, y, key, ...)
+  
+  # identify the keys that didn't match and change their names
+  # from the .x suffix to the original name. This is done
+  # for recursive merging which depends on the other 
+  # original names
+  remain <- setdiff(key_vars, key)
+  for (i in remain) names(df) <- gsub(paste0(i, "\\.x"), i, names(df))
+  
+  df
+}
+
+key_variables <- c("id_alumno", "id_gradoaula", "LlavePaisCentro")
+
+## EVerything below this line is experimental
+p <- lapply(all_data, function(x) {
+  Reduce(function(x, y) joiner(x, y, key_variables), x)
+  })
+
+p[[1]]
 
 
-t <- list(all_data[[1]][[1]], all_data[[1]][[2]], all_data[[1]][[3]], all_data[[1]][[4]])
-p <- Reduce(function(x, y) full_join(x, y, by = "id_alumno"), t)
+serce <- lapply(all_data, function(x) {
+  Reduce(function(x, y) full_join(x, y), x)
+})
+
+serce2 <- Reduce(function(x, y) full_join(x, y), serce)
 
 
+# Here's the problem up to now:
+# We join the student and family questionnaire:
+AF <- full_join(all_data[[2]][[1]], all_data[[2]][[2]], c("id_alumno", "id_gradoaula"))
+# EVerything is fine:
+dim(AF)
+
+# We then join the math, language and science datasets (in that order):
+notas <- full_join(all_data[[2]][[4]], all_data[[2]][[5]], c("id_alumno", "id_gradoaula"))
+notas <- full_join(notas, all_data[[2]][[3]], c("id_alumno", "id_gradoaula"))
+# Everything is fine:
+dim(notas)
+
+# We merge them together:
+notas_AF <- full_join(AF, notas, c("id_alumno", "id_gradoaula"))
+dim(notas_AF)
+
+# Everything is fine up to now. Because we have the same respondents across all
+# data sets. However, when we start merging the teacher data, observations
+# start to grow exponentially to little over 500,000. This is
+# obviously because some students are being duplicated due to more than 1 teacher
+# within a classroom.
+
+# For example, we have student A in Class 1. Class 1 has three teachers.
+# When we merge student A with the three rows from Class 1, student 1
+# gets duplicated 3 times. This happens because the children data doesn't
+# have the unique id for teachers, but only for class rooms. If we could
+# match students to teachers, then the info from the teachers would
+# simply be added as columns instead of duplication.
 
 
-o <- list(p, all_data[[1]][[5]], all_data[[1]][[6]], all_data[[1]][[7]])
-s <- Reduce(function(x, y) full_join(x, y, by = c("id_gradoaula.x", "id_gradoaula"), o))
-
-
-
-
-
+prof1 <- full_join(notas_AF, all_data[[2]][[6]], "id_gradoaula")
+dim(prof1)
+prof1 <- full_join(prof1, all_data[[2]][[7]], "id_gradoaula")
+dim(prof1)
+prof1 <- full_join(prof1, all_data[[2]][[8]], "id_gradoaula")
+dim(prof1)
+prof1 <- full_join(prof1, all_data[[2]][[9]], "id_gradoaula")
+dim(prof1)
